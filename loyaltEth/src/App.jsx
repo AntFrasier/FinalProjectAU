7500e47e24efc004f3d05caf90c640625707c9b5
AntFrasier <frasier@hotmail.fr>
2023-02-06 20:29:42 +0100

    Installed chakraUi

--- loyaltEth/src/App.jsx
+++ loyaltEth/src/App.jsx
@@ -3,19 +3,29 @@ import {
   modalConnectors,
   walletConnectProvider,
 } from "@web3modal/ethereum";
-
+import { ChakraProvider } from '@chakra-ui/react';
+import { extendTheme } from '@chakra-ui/react';
 import { Web3Modal, Web3Button } from "@web3modal/react";
-
 import { configureChains, createClient, WagmiConfig } from "wagmi";
-
 import { goerli } from "wagmi/chains";
 import Header from "./component/Header";
 
-const chains = [goerli];
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+}
 
+const theme = extendTheme({ colors })
+
+const chains = [goerli];
+// const wagmiKey = process.env.REACT_APP_WAGMI_KEY;
+const wagmiKey = "6b4595a029164e67c18d4f47cb371a9e";
 // Wagmi client
 const { provider } = configureChains(chains, [
-  walletConnectProvider({ projectId: "" }),
+  walletConnectProvider({ projectId: wagmiKey }),
 ]);
 const wagmiClient = createClient({
   autoConnect: true,
@@ -35,17 +45,15 @@ console.log(wagmiClient)
 function App() {
    return (
     <>
-    <WagmiConfig client={wagmiClient}>
-      <Header />
-       
+    <ChakraProvider theme={theme}>
+      <WagmiConfig client={wagmiClient}>
+        <Header />
         <Web3Modal
-      projectId= ""
-      ethereumClient={ethereumClient}
-    />
-      
-    </WagmiConfig>
-
-    
+        projectId= {wagmiKey}
+        ethereumClient={ethereumClient}
+        />
+      </WagmiConfig>
+    </ChakraProvider>
   </>
   )
 }
--- loyaltEth/src/component/Header.jsx
+++ loyaltEth/src/component/Header.jsx
@@ -1,14 +1,34 @@
-import { Web3Modal, Web3Button } from "@web3modal/react"
+import { Web3Button } from "@web3modal/react";
+import {
+        Box,
+        Heading,
+        Link,
+        Menu,
+        MenuItem,
+        MenuList,
+} from "@chakra-ui/react"
 
 export default function Header(){
 
 return (
-    <>
-        <h1>RoyaltEth</h1>
+    <>  
+    <Box w="100%" h="32px" display="flex" alignItems="center" justifyContent="space-between" p={15} mt={15}>
+        <Box display="flex" w="80%" alignItems="center">
+            <Heading as='h1'>RoyaltEth</Heading>
+            <Box display="flex" alignItems="center" placeSelf="flex-end" justifyContent="space-between">
+                <Link>
+                    About
+                </Link>
+                <Link>
+                    Partners
+                </Link>
+            </Box>
+        </Box>
         <Web3Button 
         icon="hide"
         balance="show"
         />
+    </Box>
     </>
 )
 }
