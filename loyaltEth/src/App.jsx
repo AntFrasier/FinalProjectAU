7ff50c1871e690e4cc16c5888e6896a413bd9490
realcee <frasier@hotmail.fr>
2023-03-08 16:26:32 +0100

    WIP SIWE

--- loyaltEth/src/App.jsx
+++ loyaltEth/src/App.jsx
@@ -7,12 +7,13 @@ import { Box, ChakraProvider } from '@chakra-ui/react';
 import { extendTheme } from '@chakra-ui/react';
 import { Web3Modal, Web3Button } from "@web3modal/react";
 import { configureChains, createClient, WagmiConfig } from "wagmi";
-import { goerli } from "wagmi/chains";
+import { mainnet, goerli } from "wagmi/chains";
 import Index from "./pages/Index";
 import About from "./pages/About";
 import Header from "./component/Header";
 import { Route, Routes } from 'react-router-dom';
 import PartnerRegistration from "./pages/PartnerRegistration";
+import { publicProvider } from 'wagmi/providers/public'
 
 
 const colors = {
@@ -25,18 +26,20 @@ const colors = {
 
 const theme = extendTheme({ colors })
 
-const chains = [goerli];
+const chains = [mainnet, goerli];
 const wagmiKey = import.meta.env.VITE_WAGMI_KEY;
+const w3ModalProjectId = import.meta.env.VITE_W3MODAL_PROJECT_ID;
 // Wagmi client
 const { provider } = configureChains(chains, [
   walletConnectProvider({ projectId: wagmiKey }),
+  publicProvider(),
 ]);
 const wagmiClient = createClient({ //
   autoConnect: true,
   connectors: modalConnectors({
-    projectId: "",
-    version: "1" | "2",
-    appName: "web3Modal",
+    projectId: w3ModalProjectId,
+    version: "2",
+    appName: "AuFinalProject",
     chains,
   }),
   provider,
--- loyaltEth/src/pages/Index.jsx
+++ loyaltEth/src/pages/Index.jsx
@@ -1,6 +1,7 @@
 import { Box } from '@chakra-ui/react';
 import React from 'react';
 import Register from '../component/Register';
+import { useAccount, useConnect, useEnsName } from 'wagmi'
 
 
 
@@ -8,12 +9,15 @@ import Register from '../component/Register';
 
 
 const Index = () => {
-    
-  return (
-    <Box>
-        <Register />
-    </Box>
-  )
+  const { address, isConnected } = useAccount()
+  const { data: ensName } = useEnsName({ address })
+  if (isConnected) return <div>Connected to {ensName ?? address}</div>
+  else {
+    return (
+      <Box>
+          <Register />
+      </Box>
+   )}
 }
 
 export default Index
