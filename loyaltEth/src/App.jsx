4326ebd01c339c0441d63a1b0bc7955f56b8d8e0
CMaranber <84241354+AntFrasier@users.noreply.github.com>
2023-08-29 16:38:02 +0200

    updated w3c wagmi to latest

--- loyaltEth/src/App.jsx
+++ loyaltEth/src/App.jsx
@@ -1,13 +1,14 @@
 import {
   EthereumClient,
-  modalConnectors,
-  walletConnectProvider,
+  w3mConnectors,
+  w3mProvider ,
 } from "@web3modal/ethereum";
 import { Box, ChakraProvider } from '@chakra-ui/react';
 import { extendTheme } from '@chakra-ui/react';
 import { Web3Modal, Web3Button } from "@web3modal/react";
-import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
 import { goerli } from "wagmi/chains";
+
 import Index from "./pages/Index";
 import About from "./pages/About";
 import Header from "./component/Header";
@@ -26,31 +27,41 @@ const colors = {
 const theme = extendTheme({ colors })
 
 const chains = [goerli];
+
+const projectId = '34a5caafeb6d780a6b52114437dd57b6'
+
+const { publicClient } = configureChains(chains, [w3mProvider({ projectId })])
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors: w3mConnectors({ projectId, chains }),
+  publicClient
+})
+const ethereumClient = new EthereumClient(wagmiConfig, chains)
 const wagmiKey = import.meta.env.VITE_WAGMI_KEY;
 // Wagmi client
-const { provider } = configureChains(chains, [
-  walletConnectProvider({ projectId: wagmiKey }),
-]);
-const wagmiClient = createClient({ //
-  autoConnect: true,
-  connectors: modalConnectors({
-    projectId: "",
-    version: "1" | "2",
-    appName: "web3Modal",
-    chains,
-  }),
-  provider,
-});
+// const { provider } = configureChains(chains, [
+//   walletConnectProvider({ projectId: wagmiKey }),
+// ]);
+// const wagmiClient = createClient({ //
+//   autoConnect: true,
+//   connectors: modalConnectors({
+//     projectId: "",
+//     version: "1" | "2",
+//     appName: "web3Modal",
+//     chains,
+//   }),
+//   provider,
+// });
 
-// Web3Modal Ethereum Client
-const ethereumClient = new EthereumClient(wagmiClient, chains);
+// // Web3Modal Ethereum Client
+// const ethereumClient = new EthereumClient(wagmiClient, chains);
 
-console.log(wagmiClient)
+console.log(publicClient)
 function App() {
    return (
     <>
     <ChakraProvider theme={theme}>
-      <WagmiConfig client={wagmiClient}>
+      <WagmiConfig config={wagmiConfig}>
       <Header />
       <Box pl={15}>
           <Routes>
@@ -59,7 +70,7 @@ function App() {
               <Route path="/partnerRegistration" element = {<PartnerRegistration />} />
           </Routes>
           <Web3Modal
-          projectId= {wagmiKey}
+          projectId= {projectId}
           ethereumClient={ethereumClient}
           />
         </Box>
