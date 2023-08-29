f68f71c8c6ff9c8aa10faa2200008ec0619f8708
CMaranber <84241354+AntFrasier@users.noreply.github.com>
2023-08-29 18:08:20 +0200

    add localhost to cors

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
     // 'https://www.yoursite.com',
     'http://127.0.0.1:33550',
     'http://localhost:3500',
+    'http://localhost:5173',
     'http://127.0.0.1:5173' //vite react loca
 ];
 
