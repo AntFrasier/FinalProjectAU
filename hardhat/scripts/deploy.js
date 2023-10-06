6da6fbfd1519ce37a30f92fa5ff7c67eff2e794e
CMaranber <84241354+AntFrasier@users.noreply.github.com>
2023-10-06 19:21:03 +0200

    seted up debloy factory contract

--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -7,19 +7,15 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  const LoyaltEthFactory = await hre.ethers.getContractFactory("LoyaltEthFactory");
+  const loyaltEthFactoryContract = await LoyaltEthFactory.deploy();
 
-  await lock.deployed();
+  await loyaltEthFactoryContract.deployed();
 
   console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+    `LoyaltEthFactory deployed to ${loyaltEthFactoryContract.address}`
   );
 }
 
