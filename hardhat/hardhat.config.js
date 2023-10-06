406dab411ee51be991613c8462a9f18d3156ab93
CMaranber <84241354+AntFrasier@users.noreply.github.com>
2023-10-06 19:19:45 +0200

    modified hardhat config

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -6,7 +6,19 @@ const pKey = process.env.PRIVATE_KEY
 
 module.exports = {
   defaultNetwork,
-  solidity: "0.8.17",
+  solidity:
+    {
+      version: "0.8.17",
+      settings: {
+        optimizer: {
+          enabled: true,
+          // https://docs.soliditylang.org/en/latest/using-the-compiler.html#optimizer-options
+          runs: 200,
+        },
+        viaIR: true, //added to avoid compilation error Stack too deep
+      },
+    
+    },
   networks: {
     localhost: {
       url: "http://127.0.0.1:8545/",
