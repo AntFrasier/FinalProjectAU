33a2435122e6ffa16b3cf4a6007035d0dba84f57
CMaranber <84241354+AntFrasier@users.noreply.github.com>
2023-10-09 14:34:30 +0200

    littele refactor

--- loyaltEth/src/component/AddCampagn.jsx
+++ loyaltEth/src/component/AddCampagn.jsx
@@ -1,12 +1,23 @@
-import { Button } from "@chakra-ui/react";
 import { useState } from "react";
+import {
+    Flex, 
+    Heading, 
+    Input, 
+    Text,
+    FormControl,
+    FormLabel,
+    InputGroup,
+    InputLeftAddon,
+    Button,
+  } from '@chakra-ui/react';
 
 const Addcampagn = ({setModal, modal}) => {
+
    
 
     return(
-        <>
-            <Button onClick={ () => setModal(!modal)}>Add</Button>
+        <>    
+        <Button onClick={ () => setModal(!modal)}>Add</Button>
         </>
     )
 }
