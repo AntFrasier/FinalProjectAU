83d01ff4b7ddfc4718af653891f989a0455adfdf
CMaranber <84241354+AntFrasier@users.noreply.github.com>
2023-10-12 16:07:59 +0200

    /nfts route protected by jwt

--- backend/server.js
+++ backend/server.js
@@ -14,26 +14,23 @@ connectDb();
 const app = express();
 
 app.use(cors(corsOptions));
-
 app.use(logger);
 app.use(express.urlencoded({ extended : false} ));
 app.use(express.json());
 app.use(cookieParser());
 
 //routes non protected by jwt:
-
 app.use('/login', require('./routes/loginRoutes'))
 app.use('/register', require('./routes/registerRoutes'));
 app.use('/refresh', require('./routes/refreshRoutes'));
 app.use('/partners', require('./routes/partnersRoutes'));
 app.use('/campaign', require('./routes/campaignRoutes'));
 
-app.use('/nfts', require('./routes/nftsRoutes.js'));
-
 //routes protected by jwt verification
 app.use(verifyJWT);
 app.use("/logout", require('./routes/logoutRoutes'));
 app.use('/user', require('./routes/userRoutes'));
+app.use('/nfts', require('./routes/nftsRoutes.js'));
 
 
 
